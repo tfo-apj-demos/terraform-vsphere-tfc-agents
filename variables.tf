10123cecd93fd70da6ba74731f8cc87ef9c297ab
Grant Orchard <go@hashicorp.com>
2023-11-13 16:41:36 +1100

    allow tunable agent count

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "organization" {
   description = "The Terraform Cloud organization name."
   type        = string
 }
+
+variable "agent_count" {
+  description = "Number of Terraform agents to deploy."
+  type = number
+  default = 3
+}
