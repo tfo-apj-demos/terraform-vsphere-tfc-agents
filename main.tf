157e0285211dbd25fc86c4ef5665050430d14e36
Grant Orchard <go@hashicorp.com>
2023-11-13 14:41:20 +1100

    cleanup variable usage

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+data "hcp_packer_image" "this" {
+  bucket_name     = "base-ubuntu-2204"
+  channel         = "latest"
+  cloud_provider  = "vsphere"
+  region          = "Datacenter"
+}
+
 resource "tfe_agent_pool" "this" {
   name                = "gcve_agent_pool"
   organization_scoped = true
@@ -15,12 +22,14 @@ module "vm" {
   version = "~> 1.3"
 
   hostname          = "tfc-agent-${count.index}"
-  datacenter        = var.datacenter
-  cluster           = var.cluster
-  primary_datastore = var.primary_datastore
-  folder_path       = var.folder_path
-  networks          = var.networks
-  template          = var.vsphere_template_name
+  datacenter        = "Datacenter"
+  cluster           = "cluster"
+  primary_datastore = "vsanDatastore"
+  folder_path       = "demo workloads"
+  networks = {
+    "seg-general" : "dhcp"
+  }
+  template = data.hcp_packer_image.this.cloud_image_id
 
   userdata = templatefile("${path.module}/templates/userdata.yaml.tmpl", {
     agent_token = tfe_agent_token.this.token
